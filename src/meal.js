3d7465ed68314938ad2abeb2876d0f5cd8dcd489
Wil Huffman <me@wil-huffman.io>
2021-08-28 11:47:06 -0500

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,49 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`;
+}
+
+function createMenuItem(name, price, type){
+  var menuItem = {
+      name: name,
+      price: price, 
+      type: type,
+
+   }
+  return menuItem
 
 }
+function addIngredients(item, ingredients) {
+  if (!ingredients.includes(item)) {
+    ingredients.push(item)
+  }
+}
+function formatPrice(price) {
+  return `$${price.toString()}`
+}
+function decreasePrice(price) {
+  return price * .9
+}
+function createRecipe(title, ingredients, type) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+}
+
+
+
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
